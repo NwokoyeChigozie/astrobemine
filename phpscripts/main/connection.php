6b75dbe7a0cbb60c9ddd52aa286fb9036c542dca
Nwokoye Chigozie <gregoflash05@gmail.com>
2021-12-30 23:41:46 +0100

    update

--- phpscripts/main/connection.php
+++ phpscripts/main/connection.php
@@ -1,6 +1,6 @@
 <?php
 //sql connection
-$link = mysqli_connect("localhost", "astrowgf_astro_adm", "cryptoservice", "astrowgf_astro");
+$link = mysqli_connect("localhost", "astrowgf_astro_adm", "cryptoservice", "astrowgf_metastackmine");
 if(mysqli_connect_error()){
     die('ERROR: Unable to connect:' . mysqli_connect_error());
 }
